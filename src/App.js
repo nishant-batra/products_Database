a7d6cd4b4f11843fdc6b476a426cac7308e0b8c2
nishant-batra <nishantbatra2000@gmail.com>
2021-03-31 14:21:38 +0530

    add Sort and available query

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import React from 'react'
-import Cart from './Cart'
-import Navbar from './Navbar'
-import firebase from 'firebase';
+import React from "react";
+import Cart from "./Cart";
+import Navbar from "./Navbar";
+import firebase from "firebase";
 class App extends React.Component {
-  constructor()
-  {
+  constructor() {
     super();
-    this.state={
-        products:[
-        ],
-        loading: true,
+    this.state = {
+      products: [],
+      loading: true,
     };
-    this.fb=firebase.firestore();
-    this.titleref=new React.createRef();
-    this.qtyref=new React.createRef();
-    this.srcref=new React.createRef();
-    this.priceref=new React.createRef();
+    this.availableProducts = true;
+    this.showActions = true;
+    this.fb = firebase.firestore();
+    this.titleref = new React.createRef();
+    this.qtyref = new React.createRef();
+    this.srcref = new React.createRef();
+    this.priceref = new React.createRef();
+    this.catref = new React.createRef();
   }
-  componentDidMount()
-  {
-
+  componentDidMount() {
     // firebase.firestore().collection('products')
     // .get().then((snapshot)=>{
     //  // console.log(snapshot);
@@ -33,175 +32,288 @@ class App extends React.Component {
     //     loading :false,
     //   });
     // });
-    
-    this.fb.collection('products')
-    .onSnapshot((snapshot)=>{
-    const products= snapshot.docs.map((doc)=>{
-      let data=doc.data();
-      data['key']=doc.id;
+
+    this.fb.collection("products").onSnapshot((snapshot) => {
+      const products = snapshot.docs.map((doc) => {
+        let data = doc.data();
+        data["key"] = doc.id;
         return data;
       });
       this.setState({
         products: products,
-        loading :false,
+        loading: false,
       });
     });
-    
-  } 
-  
- handleIncrease=(product)=>{
+  }
 
-const{products}=this.state;
-let index=products.indexOf(product);
-// products[index].qty++;
-// this.setState ({
-// products:products,
-//});
-const docref=this.fb.collection('products').doc(products[index].key);
-docref.update({
-  qty:products[index].qty+1,
-}).then().catch((error)=>console.log(error));
- }
- handleDecrease=(product)=>{
-   const{products}=this.state;
-   let index=products.indexOf(product);
-  //  if(products[index].qty>0)
-  //  products[index].qty--;
-  //  if(products[index].qty===0){
-  //    this.handleDelete(products[index].key);
-  //    return;
-  //  }
-  //  this.setState({
-  //    products:products,
-  //  });
-  if(products[index].qty>0)
-  {
-    const docref= this.fb.collection('products').doc(products[index].key);
-    docref.update({
-      qty:products[index].qty-1,
-    }).then().catch((error)=>{
-      console.log(error);
+  handleIncrease = (product) => {
+    const { products } = this.state;
+    let index = products.indexOf(product);
+    // products[index].qty++;
+    // this.setState ({
+    // products:products,
+    //});
+    const docref = this.fb.collection("products").doc(products[index].key);
+    docref
+      .update({
+        qty: products[index].qty + 1,
+      })
+      .then()
+      .catch((error) => console.log(error));
+  };
+  handleDecrease = (product) => {
+    const { products } = this.state;
+    let index = products.indexOf(product);
+    //  if(products[index].qty>0)
+    //  products[index].qty--;
+    //  if(products[index].qty===0){
+    //    this.handleDelete(products[index].key);
+    //    return;
+    //  }
+    //  this.setState({
+    //    products:products,
+    //  });
+    if (products[index].qty > 0) {
+      const docref = this.fb.collection("products").doc(products[index].key);
+      docref
+        .update({
+          qty: products[index].qty - 1,
+        })
+        .then()
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+  handleDelete = (key) => {
+    const { products } = this.state;
+    const items = products.filter((item) => {
+      return item.key !== key;
     });
+
+    this.setState({
+      products: items,
+    });
+    const docref = this.fb.collection("products").doc(key);
+    docref.delete().then().catch();
+  };
+  getCartCount = () => {
+    const { products } = this.state;
+    let count = 0;
+    for (let i in products) {
+      count += products[i].qty;
+    }
+
+    return count;
+  };
+  getCartTotal = () => {
+    const { products } = this.state;
+    let cartTotal = 0;
+    for (let i in products) {
+      cartTotal += products[i].qty * products[i].price;
+    }
+    return cartTotal;
+  };
+  addProducts = () => {
+    if (this.titleref.value === "") {
+      window.alert("Please fill the title field");
+      return;
+    }
+    if (this.srcref.value === "") {
+      window.alert("Please fill the src field");
+      return;
+    }
+    if (this.qtyref.value === "") {
+      window.alert("Please fill the qty field");
+      return;
+    }
+    if (this.priceref.value === "") {
+      window.alert("Please fill the price field");
+      return;
+    }
+    this.fb
+      .collection("products")
+      .add({
+        src: this.srcref.value,
+        qty: parseInt(this.qtyref.value),
+        price: parseInt(this.priceref.value),
+        title: this.titleref.value,
+        category: this.catref.value,
+      })
+      .then(() => {
+        console.log("added");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    //console.log(this.titleref.value);
+    this.srcref.value = "";
+    this.qtyref.value = "";
+    this.titleref.value = "";
+    this.priceref.value = "";
+  };
+  showAvailable = () => {
+    firebase
+      .firestore()
+      .collection("products")
+      .where("qty", ">", 0)
+      .get()
+      .then((snapshot) => {
+        // console.log(snapshot);
+        const products = snapshot.docs.map((doc) => {
+          let data = doc.data();
+          data["key"] = doc.id;
+          return data;
+        });
+        this.setState({
+          products: products,
+          loading: false,
+        });
+      });
+    this.availableProducts = false;
+    this.showActions = false;
+  };
+  showAll = () => {
+    firebase
+      .firestore()
+      .collection("products")
+      .get()
+      .then((snapshot) => {
+        // console.log(snapshot);
+        const products = snapshot.docs.map((doc) => {
+          let data = doc.data();
+          data["key"] = doc.id;
+          return data;
+        });
+        this.setState({
+          products: products,
+          loading: false,
+        });
+      });
+    this.availableProducts = true;
+    this.showActions = true;
+  };
+  sortByPrice = () => {
+    firebase
+      .firestore()
+      .collection("products")
+      .orderBy("price")
+      .get()
+      .then((snapshot) => {
+        // console.log(snapshot);
+        const products = snapshot.docs.map((doc) => {
+          let data = doc.data();
+          data["key"] = doc.id;
+          return data;
+        });
+        this.setState({
+          products: products,
+          loading: false,
+        });
+      });
+    // this.availableProducts = true;
+    // this.showActions = true;
+  };
+
+  render() {
+    const { products, loading } = this.state;
+    return (
+      <div className="App">
+        <Navbar count={this.getCartCount()} />
+        <Cart
+          products={products}
+          onIncQty={this.handleIncrease}
+          onDecQty={this.handleDecrease}
+          onDelete={this.handleDelete}
+          availableProducts={this.showActions}
+        />
+        {/* <button type="button" onClick={this.addProducts}> Add Products</button> */}
+        {loading && <h1>Loading Products...</h1>}
+        <div className="cartTotal">TOTAL: {this.getCartTotal()}</div>
+        <form className="filter">
+          Filter by Categories
+          <br />
+          <input type="radio" name="categories" value="mobile" />
+          Mobile
+          <br />
+          <input type="radio" name="categories" value="washing machine" />
+          Washing Machine
+          <br />
+          <input type="radio" name="categories" value="laptop" />
+          Laptop
+          <br />
+          <input type="radio" name="categories" value="watch" />
+          Watch
+          <br />
+          <button type="button">Filter</button>
+          <button type="button" onClick={this.sortByPrice}>
+            Sort by Price
+          </button>
+          {this.availableProducts ? (
+            <button type="button" onClick={this.showAvailable}>
+              Show only available Products
+            </button>
+          ) : (
+            <button type="button" onClick={this.showAll}>
+              Show all Products
+            </button>
+          )}
+        </form>
+
+        <form className="form-div">
+          <label for="title">Title: </label>
+          <input
+            id="title"
+            name="title"
+            type="text"
+            ref={(val) => (this.titleref = val)}
+            required
+          ></input>
+          <label for="src"> Image Source: </label>
+          <input
+            id="src"
+            name="src"
+            type="url"
+            ref={(val) => {
+              this.srcref = val;
+            }}
+            required
+          ></input>
+          <label for="qty">Qty: </label>
+          <input
+            id="qty"
+            name="qty"
+            type="number"
+            ref={(val) => {
+              this.qtyref = val;
+            }}
+            required
+          />
+          <label for="price">Price: </label>
+          <input
+            id="price"
+            name="price"
+            type="number"
+            ref={(val) => {
+              this.priceref = val;
+            }}
+            required
+          ></input>
+          <label for="cat">Category: </label>
+          <input
+            id="cat"
+            name="cat"
+            type="text"
+            ref={(val) => (this.catref = val)}
+            required
+          ></input>
+          <button type="button" onClick={this.addProducts}>
+            Add Products
+          </button>
+        </form>
+      </div>
+    );
   }
-  
- }
- handleDelete=(key)=>{
-   const {products}=this.state;
-   const items = products.filter((item)=>{return item.key!==key});
-
-   this.setState({
-     products: items,
-   });
-   const docref= this.fb.collection('products').doc(key);
-   docref.delete().then().catch();
- }
- getCartCount=()=>{
-const {products}=this.state;
-let count=0;
-for( let i in products)
-{
-
-count+=products[i].qty;
-}
-
-return count;
- }
- getCartTotal=()=>{
-  const {products}=this.state; 
-  let cartTotal=0;
-   for( let i in products)
-   {
-     cartTotal+=products[i].qty*products[i].price;
-   }
-   return cartTotal;
- }
- addProducts=()=>{
-   if(this.titleref.value==="")
-   {
-   window.alert("Please fill the title field");
-  return; 
-  }
-  if(this.srcref.value==="")
-  {
-  window.alert("Please fill the src field");
- return; 
- }
- if(this.qtyref.value==="")
- {
- window.alert("Please fill the qty field");
-return; 
-}
-if(this.priceref.value==="")
-{
-window.alert("Please fill the price field");
-return; 
-}
-this.fb.collection('products').add({
-  src:this.srcref.value,
-  qty:this.qtyref.value,
-  price:this.priceref.value,
-  title:this.titleref.value,
-}).then(()=>{console.log("added")}).catch((error)=>{console.log(error)});
-//console.log(this.titleref.value);
-this.srcref.value="";
-this.qtyref.value="";
-this.titleref.value="";
-this.priceref.value="";
-
- }
- 
-  render()
-  {
-    const {products,loading}=this.state;
-  return (
-    <div className="App">
-      <Navbar count={this.getCartCount()}/>
-     <Cart 
-     products={products}
-      onIncQty={this.handleIncrease}
-      onDecQty={
-      this.handleDecrease
-      }
-      onDelete={this.handleDelete}
-     />
-     {/* <button type="button" onClick={this.addProducts}> Add Products</button> */}
-     {loading && <h1>Loading Products...</h1>}
-     <div className="cartTotal">TOTAL:  {this.getCartTotal()}</div>
-     <form className="form-div"> 
-       <label for="title">Title: </label>
-       <input id="title" 
-       name="title"
-        type="text"
-        ref={(val)=>this.titleref=val}
-        required></input>
-       <label for="src"> Image Source: </label>
-       <input id="src" name="src" type="url"
-         ref={(val)=>{
-          this.srcref=val;
-        }}
-        required></input>
-       <label for="qty" >Qty: </label>
-       <input id="qty" name="qty" type="number"
-       ref={(val)=>{
-        this.qtyref=val;
-      }
-
-      }
-      required/>
-       <label for="price" >Price: </label>
-       <input id="price" name="price" type="number"
-       ref={(val)=>{
-        this.priceref=val;
-      }
-
-      }
-      required></input>
-       <button type="button" onClick={this.addProducts}> Add Products</button>
-     </form>
-    
-    </div>
-  );
-}
 }
 
 export default App;
--- src/Cart.js
+++ src/Cart.js
@@ -1,32 +1,23 @@
-import React from 'react';
-import CartItem from './CartItem'
-const Cart =(props)=> {
-   
-
-    const {products}=props;
+import React from "react";
+import CartItem from "./CartItem";
+const Cart = (props) => {
+  const { products } = props;
   return (
-       
-      <div className="cart">
-     {products.map((product)=>
-     {
-return<CartItem product={product}
- key={product.key}
-   onIncQty={props.onIncQty}
-   onDecQty={
-   props.onDecQty
-   }
-   onDelete={props.onDelete}
-   >
- </CartItem>
-     }
-     )
-     }
-   
-      </div>
+    <div className="cart">
+      {products.map((product) => {
+        return (
+          <CartItem
+            product={product}
+            key={product.key}
+            onIncQty={props.onIncQty}
+            onDecQty={props.onDecQty}
+            onDelete={props.onDelete}
+            availableProducts={props.availableProducts}
+          ></CartItem>
+        );
+      })}
+    </div>
   );
-
-}
-
-
+};
 
 export default Cart;
